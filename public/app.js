aba8a3e948677b4f61ee7ed1b5253ad2e69a1a7e
jasminejeane <jasminejeane@gmail.com>
2018-03-25 16:30:22 -0700

    testing chart update

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 let numRows;
-
+var mainChart;
 $(document).ready(function() {
 
   // show more show less for length of rows
@@ -28,11 +28,9 @@ $(document).ready(function() {
   });
 });
 
-
 // updates animal type on button click
 function updateType() {
   $('input[type=checkbox]:checked').each(function() {
-
 
     const id = this.attributes[0].value;
     const type = this.attributes[1].value;
@@ -40,8 +38,8 @@ function updateType() {
       ? 'dog'
       : 'cat';
     const changeEmoji = (type === 'cat')
-          ? '&#x1F436'
-          : '&#x1F42F';
+      ? '&#x1F436'
+      : '&#x1F42F';
 
     $("#type" + id).html(changeEmoji);
 
@@ -54,45 +52,71 @@ function updateType() {
         type: changeType
       }
     }).then(function() {
+      console.log("updated", id);
     });
 
+    // var s1 = mainChart.get('dogs');
+    // var points = s1.data;
+    // var firstPoint = points[0];
+    // firstPoint.remove();
+    // s1.addPoint({
+    //   name: 'dog',
+    //    y: 4,
+    //    x: 1
+    //  });
+    // console.log("dogs point", points);
+    // mainChart.redraw();
+    // https://github.com/highcharts/highcharts/issues/5318
+    // https://jsfiddle.net/tpu0fwpe/3/
   });
 }
 // toggles side bar
 const menuClick = () => {
-  $("#wrapper").toggleClass("active");
-}
-
-const dataDogs = [],
-      dataCats = [],
-      serieName = [];
-
-// create Characteristics Table
-const createTable = (data, min, max) => {
-
-  if (!min && !max) {
-    min = 0;
-    max = 25;
+    $("#wrapper").toggleClass("active");
   }
 
-  $("#animal-table > tbody").html("");
-  for (let i = min; i < max; i++) {
+  const dataDogs = [],
+    dataCats = [],
+    serieName = [];
 
-    const { id, type, name, body_size, claws, color, fur_type, number_of_legs } = data[i];
-		let aniEmoji;
+  // create Characteristics Table
+  const createTable = (data, min, max) => {
 
-
-		let serieData = {	x: id, y: number_of_legs, nameVal: name}
-
-    if (type === "dog") {
-      dataDogs.push(serieData);
-			aniEmoji = '&#x1F436';
-    } else if (type === "cat") {
-      dataCats.push(serieData);
-			aniEmoji = '&#x1F42F';
+    if (!min && !max) {
+      min = 0;
+      max = 25;
     }
 
-    $("#animal-table > tbody").append(`<tr>
+    $("#animal-table > tbody").html("");
+    for (let i = min; i < max; i++) {
+
+      const {
+        id,
+        type,
+        name,
+        body_size,
+        claws,
+        color,
+        fur_type,
+        number_of_legs
+      } = data[i];
+      let aniEmoji;
+
+      let serieData = {
+        x: id,
+        y: number_of_legs,
+        nameVal: name
+      }
+
+      if (type === "dog") {
+        dataDogs.push(serieData);
+        aniEmoji = '&#x1F436';
+      } else if (type === "cat") {
+        dataCats.push(serieData);
+        aniEmoji = '&#x1F42F';
+      }
+
+      $("#animal-table > tbody").append(`<tr>
 			<td>
 			<input data-id=${id} data-type=${type} type="checkbox" name="checkbox" value="checkbox">
 			</td>
@@ -105,115 +129,114 @@ const createTable = (data, min, max) => {
 			<td>${fur_type} </td>
 			<td>${number_of_legs}</td>
 			</tr>`);
+    }
+
+    numRows = $("#animal-table tr").length;
+    // console.log(numRows);
   }
 
-  numRows = $("#animal-table tr").length;
-  console.log(numRows);
-}
+  const renderTable = (min, max) => {
+    $.ajax({url: "http://localhost:8080/animals", method: "GET"}).then(function(data) {
+      createTable(data, min, max);
+    });
+  }
 
-
-const renderTable = (min, max) => {
+  // create initial chart and table
   $.ajax({url: "http://localhost:8080/animals", method: "GET"}).then(function(data) {
-    createTable(data, min, max);
-  });
-}
 
-// create initial chart and table
-$.ajax({url: "http://localhost:8080/animals", method: "GET"}).then(function(data) {
+    createTable(data, 0, 25);
 
-  createTable(data, 0, 25);
-
-  Highcharts.chart('container', {
-    chart: {
-      type: 'scatter',
-      zoomType: 'xy',
-			plotBackgroundColor: '#1f2237',
-			plotBorderWidth: null,
-			plotShadow: true,
-			backgroundColor: 'transparent',
-      // events: {click: function(e){
-      //   console.log(e);
-      // }}
-    },
-    title: {
-      text: 'Number of Legs vs. IDs of 2600 Animals by Dog or Cat Type',
-			style: {
-				color: '#fff'
-			}
-
-    },
-    subtitle: {
-      text: 'Source: Animals API',
-			color: '#fff'
-    },
-    xAxis: {
-      title: {
-        enabled: true,
-        text: 'Animal IDs'
-      },
-      minRange: 1,
-      allowDecimals: false,
-      startOnTick: true,
-      endOnTick: true,
-      showLastLabel: true,
-      events: {
-        setExtremes: function(e) {
-          renderTable(Math.ceil(e.min), Math.ceil(e.max));
+    mainChart = Highcharts.chart('container', {
+      chart: {
+        type: 'scatter',
+        zoomType: 'xy',
+        plotBackgroundColor: '#1f2237',
+        plotBorderWidth: null,
+        plotShadow: true,
+        backgroundColor: 'transparent',
+        events: {
+          update: function(e) {
+            console.log("updated", e);
+          }
         }
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Number of Legs'
+
       },
-      tickInterval: 1,
-      minRange: 1,
-      allowDecimals: false
-    },
-    legend: {
+      title: {
+        text: 'Number of Legs vs. IDs of 2600 Animals by Dog or Cat Type',
+        style: {
+          color: '#fff'
+        }
 
-      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-      borderWidth: 1
-    },
-    plotOptions: {
-      scatter: {
-        marker: {
-          radius: 5,
-          states: {
-            hover: {
-              enabled: true,
-              lineColor: '#2c2f59',
+      },
+      subtitle: {
+        text: 'Source: Animals API',
+        color: '#fff'
+      },
+      xAxis: {
+        title: {
+          enabled: true,
+          text: 'Animal IDs'
+        },
+        minRange: 1,
+        allowDecimals: false,
+        startOnTick: true,
+        endOnTick: true,
+        showLastLabel: true,
+        events: {
+          setExtremes: function(e) {
+            renderTable(Math.ceil(e.min), Math.ceil(e.max));
+          }
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Number of Legs'
+        },
+        tickInterval: 1,
+        minRange: 1,
+        allowDecimals: false
+      },
+      legend: {
 
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor),
+        borderWidth: 1
+      },
+      plotOptions: {
+        scatter: {
+          marker: {
+            radius: 5,
+            states: {
+              hover: {
+                enabled: true,
+                lineColor: '#2c2f59'
+              }
+            }
+          },
+          tooltip: {
+
+            pointFormatter: function() {
+              return `ID: <b>${this.x} </b><br>
+							Name: <b> ${this.nameVal}</b><br>`;
             }
           }
-        },
-        tooltip: {
-
-          pointFormatter: function(){
-						// console.log(this);
-						return `ID: <b>${this.x} </b><br>
-							Name: <b> ${this.nameVal}</b><br>`;
-          }
         }
-      }
-    },
-    series: [
-      {
-        name: 'Dog',
-        color: '#ccff00',
-				turboThreshold: 3000,
-        data: dataDogs
+      },
+      series: [
+        {
+          id: 'dogs',
+          name: 'Dog',
+          color: '#ccff00',
+          turboThreshold: 3000,
+          data: dataDogs
 
-      }, {
-        name: 'Cat',
-        color: '#ff00cc',
-				turboThreshold: 3000,
-        data: dataCats
-      }
+        }, {
+          id: 'cats',
+          name: 'Cat',
+          color: '#ff00cc',
+          turboThreshold: 3000,
+          data: dataCats
+        }
 
-    ]
+      ]
+    });
   });
-});
-// for rendering outside of mulitple ajax calls
-// see color-combo in high-charts-1
-// $('#container').highcharts(json);
--- test/testing.js
+++ test/testing.js
@@ -1,10 +1,72 @@
 'use strict';
 
-var should = require('chai').should();
+const expect = require("chai").expect;
+const app = require('../public/app');
 
+
+const animalData = [
+{
+body_size: "large",
+claws: "sharp",
+color: "brown",
+fur_type: "plain",
+id: 1,
+name: "Peanut butter",
+number_of_legs: 4,
+type: "dog"
+},
+{
+body_size: "normal",
+claws: "sharp",
+color: "black",
+fur_type: "striped",
+id: 2,
+name: "Teddy bear biscuit",
+number_of_legs: 4,
+type: "cat"
+},
+{
+body_size: "large",
+claws: "sharp",
+color: "white",
+fur_type: "striped",
+id: 3,
+name: "Albert Pamplemousse",
+number_of_legs: 4,
+type: "dog"
+},
+{
+body_size: "small",
+claws: "sharp",
+color: "white",
+fur_type: "striped",
+id: 4,
+name: "Apricot Strudel",
+number_of_legs: 4,
+type: "dog"
+},
+{
+body_size: "normal",
+claws: "sharp",
+color: "white",
+fur_type: "striped",
+id: 5,
+name: "Baron Von Thumper",
+number_of_legs: 4,
+type: "cat"
+}
+]
 
 describe('Universe', function(){
   it('should be self-consistent', function (){
-    (1).should.equal(2);
+    (2).should.equal(2);
+  })
+})
+
+
+describe('Create Table', function(){
+  it('render all Characteristics', function (){
+
+    expect(createTable(animalData, 0, 5)).to.include("white");
   })
 })
