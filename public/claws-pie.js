83a4b74d8fa89481312430345994118b22f8ff5c
jasminejeane <jasminejeane@gmail.com>
2018-03-25 11:22:39 -0700

    tested obj breakdown on claws chart

--- public/claws-pie.js
+++ public/claws-pie.js
@@ -25,47 +25,53 @@ $(function() {
       }
     }
 
-    Highcharts.chart('claws', {
-      chart: {
-        type: 'pie',
-        margin: [0, 0, 0, 0],
+
+    const chart = {
+      type: 'pie',
+      margin: [
+        0, 0, 0, 0
+      ],
       plotBackgroundColor: '#1f2237',
       plotBorderWidth: null,
       plotShadow: true
-      },
-      // options3d: {
-      //   enabled: true,
-      //   alpha: 45,
-      //   beta: 0
-      // },
-      subtitle: {
-        text: 'CLAWS',
-        style: {
-          color: '#fff',
-         fontWeight: 'bold',
-         fontSize: 16
-     }
-      },
-      plotOptions: {
-        series: {
-          dataLabels: {
-            enabled: true,
-            crop: false,
-            overflow: "none",
-            connectorWidth: 0,
-            distance: -30,
-            format: '{point.name} <br> {point.percentage:.1f}%',
-            style: {
-                  textOutline: false
-              }
+    };
+
+    const subTitleStlye = {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize: 16
+    };
+
+    const plotOptions = {
+      series: {
+        dataLabels: {
+          enabled: true,
+          crop: false,
+          overflow: "none",
+          connectorWidth: 0,
+          distance: -30,
+          format: '{point.name} <br> {point.percentage:.1f}%',
+          style: {
+            textOutline: false
           }
         }
-      },
+      }
+    };
 
-      tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.percentage:.1f}%</b> of total<br/>'
+    const toolTip = {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.percentage:.1f}%</b> of total<br/>'
+    }
+
+    Highcharts.chart('claws', {
+      chart: chart,
+      subtitle: {
+        text: 'CLAWS',
+        style: subTitleStlye
       },
+      plotOptions: plotOptions,
+
+      tooltip: toolTip,
 
       "series": [{
         "name": "Browsers",
